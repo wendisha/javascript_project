d506ae48541112cafe2b9611af696102533912f1
github username <github email address>
2019-04-26 17:00:49 -0400

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :contracts
   resources :futbolistas do
     resources :agents, only: [:show, :new]
-    resources :contracts, only: [:index]
   end
   resources :sessions, only: [:create, :destroy]
-
+  resources :users, only: [:new, :create, :show] do
+    resources :contracts, only: [:index]
+  end 
 end

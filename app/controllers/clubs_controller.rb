b3cc704655a41645938ab156615623ccad6ad687
github username <github email address>
2019-04-26 16:51:44 -0400

    work on clubs controller

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,3 +1,27 @@
 class ClubsController < ApplicationController
 
-end 
+  def index
+    @clubs = Club.all
+  end
+
+  def show
+    @club = Club.find_by_id(params[:id])
+  end
+
+  def new
+    @club = Club.new
+  end
+
+  def create
+
+  end
+
+  def edit
+    @club = Club.find_by_id(params[:id])
+  end
+
+  def update
+
+  end 
+
+end

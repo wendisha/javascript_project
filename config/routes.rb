2754a473f54d44505ea600eb61214fd84e424e6c
github username <github email address>
2019-04-28 03:03:50 -0400

    change routes to have user as parent

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  resources :agents, only: [:create, :show, :index, :edit, :update, :destroy]
-  resources :clubs
-  resources :contracts, only: [:show, :new, :create, :edit, :update]
-  resources :futbolistas do
-    resources :agents, only: [:new]
-  end
+
+
   resources :users, only: [:new, :create, :show] do
-    resources :contracts, only: [:index]
+    resources :contracts
+    resources :agents
+    resources :clubs
+    resources :futbolistas
   end
 
 end

66ec2847b5cdfb822559a5fef32eaff6450e5dff
github username <github email address>
2019-05-01 18:24:23 -0400

    update actions, _error partials

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 User.create(name: "dfs", password: "dfs")
-
-Agent.create(user_id: 1, name: "LookAtMe", country_of_origin: "selfland")
-Agent.create(user_id: 1, name: "LookAtMyClient", country_of_origin: "humbleland")
-
-Club.create(user_id: 1, name: "Barcelona", city: "Barcelona", nation: "Spain", league_division: "La Liga")
-Club.create(user_id: 1, name: "Real Madrid", city: "Madrid", nation: "Spain", league_division: "La Liga")
-
-Player.create(agent_id: 1, club_id: 1, user_id: 1, name: "Messi", country_of_origin: "Argentina", position: "Att Mid")
-Player.create(agent_id: 2, club_id: 2, user_id: 1, name: "C Ronaldo", country_of_origin: "Portugal", position: "Att Mid")
+# 
+# Agent.create(user_id: 1, name: "LookAtMe", country_of_origin: "selfland")
+# Agent.create(user_id: 1, name: "LookAtMyClient", country_of_origin: "humbleland")
+#
+# Club.create(user_id: 1, name: "Barcelona", city: "Barcelona", nation: "Spain", league_division: "La Liga")
+# Club.create(user_id: 1, name: "Real Madrid", city: "Madrid", nation: "Spain", league_division: "La Liga")
+#
+# Player.create(agent_id: 1, club_id: 1, user_id: 1, name: "Messi", country_of_origin: "Argentina", position: "Att Mid")
+# Player.create(agent_id: 2, club_id: 2, user_id: 1, name: "C Ronaldo", country_of_origin: "Portugal", position: "Att Mid")
 
 Status.create(wording: "in the works")
 Status.create(wording: "pending")
